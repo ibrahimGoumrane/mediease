da7c816188cedd673e2c3b2e7ce15ead9cc13e49
raizel <ismailmouf@gmail.com>
2024-05-26 22:27:59 +0100

    finalized login & signup pages

--- main/build/view/sign-up.php
+++ main/build/view/sign-up.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $host = 'localhost';
 $dbname = 'medieaseDB';
 $username = 'root';
@@ -16,10 +19,9 @@ try {
     $email = $_POST['email'];
     $hashed_password  = password_hash($_POST['password'], PASSWORD_BCRYPT);
     $user_type = $_POST['user_type'];
-    $years_of_experience=$_POST["years_of_experience"];
-    $specialization=$_POST["specialization"];
-    $medical_history=$_POST["medical_history"];
-
+    $years_of_experience = isset($_POST["years_of_experience"]) ? $_POST["years_of_experience"] : null;
+    $specialization = isset($_POST["specialization"]) ? $_POST["specialization"] : null;
+    $medical_history = isset($_POST["medical_history"]) ? $_POST["medical_history"] : null;
 
     // Insert into Person table
     $stmt = $conn->prepare("INSERT INTO Person (full_name, date_of_birth, gender, phone_number, email, password) VALUES (?, ?, ?, ?, ?, ?)");
@@ -29,11 +31,10 @@ try {
     $person_id = $conn->lastInsertId();
 
     // Insert into Patient or Doctor table
-    if ($user_type == 'patient') {
+    if ($user_type == 'Patient') {
         $stmt = $conn->prepare("INSERT INTO Patient (id,medical_history) VALUES (?,?)");
-
         $stmt->execute([$person_id, $medical_history]);
-    } else if ($user_type == 'doctor') {
+    } else if ($user_type == 'Doctor') {
         $stmt = $conn->prepare("INSERT INTO Doctor (id,years_of_experience,specialization) VALUES (?,?,?)");
         $stmt->execute([$person_id,$years_of_experience,$specialization]);
     }
